8d6e48740a867c01e19bc83c87fb9c003dc0e328
Pat York <guitarp_11@yahoo.com>
2012-09-23 11:03:49 -0700

    Triangle implementation done

--- src/test/lab4tests.cpp
+++ src/test/lab4tests.cpp
@@ -1,70 +1,39 @@
 #include <memory>
 #include <vector>
 #include "gtest/gtest.h"
-#include "Circle.h"
-#include "Square.h"
+#include "Triangle.h"
 #include <string>
 
-const float PI = 3.14159265f;
-
-TEST(Shapes, DrawCircle) {
-  Circle c{1.0f};
-  EXPECT_EQ("drew a circle", c.draw());
+TEST(Shapes, DrawTriangle) {
+  Triangle t{1.0f, 1.0f, 1.0f};
+  EXPECT_EQ("drew a triangle", t.draw());
 }
 
-TEST(Shapes, DrawSquare) {
-  Square s{1.0f};
-  EXPECT_EQ("drew a square", s.draw());
-}
-
-TEST(ShapePtr, DrawSquare) {
+TEST(ShapePtr, DrawTriangle) {
   // change this to make the test pass
-  std::shared_ptr<Shape> s = std::make_shared<Square>(1.0f);
+  std::shared_ptr<Shape> t = std::make_shared<Triangle>(1.0f, 1.0f, 1.0f);
 
-  EXPECT_EQ("drew a square", s->draw());
+  EXPECT_EQ("drew a triangle", t->draw());
 }
 
-TEST(Shapes, BunchOfSquaresArea) {
+TEST(Shapes, BunchOfTrianglesArea) {
   // change these to make the test pass
-  std::shared_ptr<Shape> sq_ptr1 = std::make_shared<Square>(1.0f);
-  std::shared_ptr<Shape> sq_ptr2 = std::make_shared<Square>(1.0f);
-  std::shared_ptr<Shape> sq_ptr3 = std::make_shared<Square>(1.0f);
+  std::shared_ptr<Shape> t_ptr1 = std::make_shared<Triangle>(1.0f, 1.0f, 1.0f);
+  std::shared_ptr<Shape> t_ptr2 = std::make_shared<Triangle>(2.0f, 2.0f, 2.0f);
+  std::shared_ptr<Shape> t_ptr3 = std::make_shared<Triangle>(3.0f, 3.0f, 3.0f);
 
-  EXPECT_FLOAT_EQ(3.0f, sq_ptr1->area() + 
-		  sq_ptr2->area() + sq_ptr3->area());
-}
-
-TEST(Shapes, BunchOfCirclesArea) {
-  std::shared_ptr<Shape> c_ptr1 = std::make_shared<Circle>(1.0f);
-  std::shared_ptr<Shape> c_ptr2 = std::make_shared<Circle>(1.0f);
-  std::shared_ptr<Shape> c_ptr3 = std::make_shared<Circle>(1.0f);
-
-  EXPECT_FLOAT_EQ(3.0f * PI, c_ptr1->area() + 
-		  c_ptr2->area() + c_ptr3->area());
-}
-
-TEST(Shapes, DifferentShapes) {
-  // change the assignments so that the test becomes true.
-  std::shared_ptr<Shape> sq_ptr1 = std::make_shared<Square>(4.0f); 
-  std::shared_ptr<Shape> c_ptr1 = std::make_shared<Circle>(9.0f);
-  std::shared_ptr<Shape> c_ptr2 = std::make_shared<Circle>(1.0f);
-
-  EXPECT_FLOAT_EQ(4.0f * 4.0f + PI * 9.0f * 9.0f + PI,
-		  sq_ptr1->area() + c_ptr1->area() + c_ptr2->area());
+  EXPECT_FLOAT_EQ(6.0621777f, t_ptr1->area() + 
+		  t_ptr2->area() + t_ptr3->area());
 }
 
 TEST(ShapeVec, AreasSumCorrectly) {
-  std::vector<std::shared_ptr<Shape> > drawingBoard{6};
+  std::vector<std::shared_ptr<Shape> > drawingBoard{3};
 
-  // TODO Add three unit circles to the drawingBoard.
-  // TODO Add three unit squares to the drawingBoard
+  // TODO Add three unit triangles to the drawingBoard.
 
-  drawingBoard[0] = std::make_shared<Square>(1.0f);
-  drawingBoard[1] = std::make_shared<Square>(1.0f);
-  drawingBoard[2] = std::make_shared<Square>(1.0f);
-  drawingBoard[3] = std::make_shared<Circle>(1.0f);
-  drawingBoard[4] = std::make_shared<Circle>(1.0f);
-  drawingBoard[5] = std::make_shared<Circle>(1.0f);
+  drawingBoard[0] = std::make_shared<Triangle>(1.0f, 1.0f, 1.0f);
+  drawingBoard[1] = std::make_shared<Triangle>(2.0f, 2.0f, 2.0f);
+  drawingBoard[2] = std::make_shared<Triangle>(3.0f, 3.0f, 3.0f);
   
 
   float areas = 0.0f;
@@ -72,7 +41,7 @@ TEST(ShapeVec, AreasSumCorrectly) {
     areas += s->area();
   }
 
-  EXPECT_FLOAT_EQ(3.0 + 3.0 * PI, areas);
+  EXPECT_FLOAT_EQ(6.0621777f, areas);
 }
 
 
